b6854024455adad6ad274050983bdd549a0cde01
Alan <Alan@dev.lan>
2023-02-21 12:40:12 -0300

    prisma create user

--- backend/routes/users/index.ts
+++ backend/routes/users/index.ts
@@ -1,11 +1,46 @@
 import { FastifyPluginAsync } from "fastify";
 
+const bodySchema = {
+  type: "object",
+  properties: {
+    firstName: { type: "string", minLength: 2 },
+    lastName: { type: "string", minLength: 2 },
+    age: { type: "string", minLength: 1 },
+  },
+  required: ["firstName", "lastName", "age"],
+};
+
 const users: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
   fastify.get("/", async function (request, reply) {
     const users = await fastify.prisma.user.findMany();
-    console.log("====================>  ", users);
+
     return [users];
   });
+
+  fastify.post(
+    "/",
+    {
+      schema: {
+        body: bodySchema,
+      },
+    },
+    async function handler(request, reply) {
+      const { firstName, lastName, age }: any = request.body;
+      const validateRequest = request.validateInput(request.body, bodySchema);
+      if (validateRequest) {
+        console.log("=============================> ", validateRequest);
+        const result = await fastify.prisma.user.create({
+          data: {
+            firstName,
+            lastName,
+            age,
+          },
+        });
+
+        return result;
+      }
+    }
+  );
 };
 
 export default users;
